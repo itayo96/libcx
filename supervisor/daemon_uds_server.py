9ac67dfa5d9c9943f65932330dd8e97296647dbb
Omer Horev <omer.hv@gmail.com>
2020-01-16 07:38:42 -0800

    Added a basic UDS server

---
+++ supervisor/daemon_uds_server.py
@@ -0,0 +1,58 @@
+import logging
+import os
+import socket
+
+
+class DaemonUDSServer:
+    """
+    An UDS based server for the agents to communicate with. This class handles the communication and not the actual
+    logic.
+    """
+    def __init__(self, address, libc_usage_callback):
+        logging.debug("Creating daemon server", address)
+        self._create_server(address)
+
+    def run(self):
+        """
+        Starts running the server. This method is blocking
+        :return: None
+        """
+        if not self.is_open():
+            raise self.ServerNotRunning()
+
+        self._control_socket.listen(1)
+
+        logging.debug("Received a new client")
+
+        connection, client_address = self._control_socket.accept()
+
+        data = connection.recv(8)
+
+        logging.debug(data)
+
+    def is_open(self):
+        """
+        :return: Weather the server is open or not
+        """
+        return self._control_socket is not None
+
+    def _create_server(self, address):
+        """
+        Creates the server
+        :param address: The UDS control filename
+        """
+        try:
+            os.unlink(address)
+        except OSError:
+            if os.path.exists(address):
+                raise FileNotFoundError("Can't open UDS file")
+
+        self._control_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._control_socket.bind(address)
+
+    class ServerNotRunning(Exception):
+        """
+        Exceptions the occurs when operating on a closed server
+        """
+
+    _control_socket = None
--- supervisor/main.py
+++ supervisor/main.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
+from pathlib import Path
+import daemon_uds_server
+
+daemon_folder = "~/.libcx/"
+daemon_main_uds_filename = daemon_folder + "daemon.uds"
+
+
+def create_folder():
+    """
+    Creates the daemon folder if it doesnt exists
+    """
+    Path(daemon_folder).mkdir(parents=True, exist_ok=True)
+
+
+def main():
+    create_folder()
+    server = daemon_uds_server.DaemonUDSServer(daemon_main_uds_filename, lambda a : a)
+    server.run()
+
 
 if __name__ == '__main__':
-    print("Hello")
+    main()
