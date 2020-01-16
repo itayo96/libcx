602e0411ed14b6ca7ad1e611df1b2bd3ac048ea6
yair lad <yair.3001.lad@gmail.com>
2020-01-16 17:18:21 +0200

    sending connection message to daemon before program main

--- wrapper/src/libcx.cpp
+++ wrapper/src/libcx.cpp
@@ -6,13 +6,57 @@
 #include <thread>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+
+struct connection_msg
+{
+    uint32_t opcode;
+    uint32_t pid;
+};
 
 static struct main
 {
     main()
     {
+        struct sockaddr_un addr;
+        int fd, size;
+        char * sock_path = "~/.libcx/daemon.uds";
+        struct connection_msg msg;
+
+        fd = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (fd == -1)
+        {
+            printf("error creating control socket - %d\n", errno);
+            goto handle_error;
+        }
+
+        memset(&addr, 0, sizeof(addr));
+        addr.sun_family = AF_UNIX;
+        strncpy(addr.sun_path, sock_path, sizeof(sock_path));
+
+        if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1)
+        {
+            printf("error connecting control socket - %d\n", errno);
+            goto handle_error;
+        }
+
+        msg.opcode = 0x3001;
+        msg.pid = static_cast<uint32_t>(getpid());
+
+        size = write(fd, (uint8_t*)&msg, sizeof(msg));
+        if (size != sizeof(msg))
+        {
+            printf("error sending connection message - %d\n", size);
+            goto handle_error;
+        }
+
         // This code actually runs before programs main and even before its static constructors
         printf("libcx main start pid = %d\n", static_cast<int>(getpid()));
+
+    handle_error:
+        printf("exit libcx main\n");
     }
 } _;
 
